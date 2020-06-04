7b6a8060d043cfa0f0c8d7e9ba8e1d66dd7eed33
Ryan DeFever <r.s.defever@gmail.com>
2020-06-04 08:58:31 -0400

    Fix bug in command line args

--- analysis/extract_r125_density.py
+++ analysis/extract_r125_density.py
@@ -2,27 +2,27 @@ import signac
 import sys
 
 from fffit.signac import save_signac_results
-from utils.r32 import R32
+from utils.r125 import R125
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python extract_r32_density.py [iteration number]")
+        print("Usage: python extract_r125_density.py [iteration number]")
         exit(1)
     else:
         iternum = sys.argv[1]
 
-    R32 = R32()
+    R125 = R125()
 
     run_path = "/scratch365/rdefever/hfcs-fffit/runs/"
-    itername = "r32-density-iter" + str(iternum)
+    itername = "r125-density-iter" + str(iternum)
     project_path = run_path + itername
     csv_name = "csv/" + itername + ".csv"
 
     property_names = ["density"]
     project = signac.get_project(project_path)
 
-    save_signac_results(project, R32.param_names, property_names, csv_name)
+    save_signac_results(project, R125.param_names, property_names, csv_name)
 
 
 if __name__ == "__main__":
