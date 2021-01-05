d3fc67b3e6dbdb83c17882bb849fdf581e9b1898
Ryan S. DeFever <r.s.defever@gmail.com>
2021-01-05 13:53:44 -0500

    Add plots for VLE comparisons

--- hfcs-fffit/analysis/final-figs/plotfig3-r125.py
+++ hfcs-fffit/analysis/final-figs/plotfig3-r125.py
@@ -7,7 +7,9 @@ import seaborn
 
 sys.path.append("../")
 
+from scipy.stats import linregress
 from utils.r125 import R125Constants
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 
 R125 = R125Constants()
 
@@ -22,7 +24,7 @@ def main():
 
     # Plot VLE envelopes
     clrs = seaborn.color_palette('bright', n_colors=len(df))
-    np.random.seed(10)
+    np.random.seed(11)
     np.random.shuffle(clrs)
 
     fig, ax = plt.subplots()
@@ -33,55 +35,88 @@ def main():
             np.tile(temp, len(df)),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
         ax.scatter(
             df.filter(regex=(f"vap_density_{float(temp):.0f}K")),
             np.tile(temp, len(df)),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
     ax.scatter(
         df.filter(regex=("sim_rhoc")),
         df.filter(regex=("sim_Tc")),
         c=clrs,
         s=180,
-        alpha=0.1,
+        alpha=0.2,
     )
+
+    tc, rhoc = calc_critical(df_gaff)
+    ax.scatter(
+        df_gaff["rholiq_kgm3"],
+        df_gaff["T_K"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
+    ax.scatter(
+        df_gaff["rhovap_kgm3"],
+        df_gaff["T_K"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        marker='s',
+    )
+    ax.scatter(
+        rhoc,
+        tc,
+        c='gray',
+        s=120,
+        alpha=0.7,
+        marker='s',
+    )
+
     ax.scatter(
         R125.expt_liq_density.values(),
         R125.expt_liq_density.keys(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
     ax.scatter(
         R125.expt_vap_density.values(),
         R125.expt_vap_density.keys(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
-    ax.scatter(R125.expt_rhoc, R125.expt_Tc, color="black", marker="x", s=240)
+    ax.scatter(R125.expt_rhoc, R125.expt_Tc, color="black", marker="x", linewidths=2, s=200)
 
-    ax.set_ylim(220,360)
-    ax.set_yticks([240, 280, 320, 360])
-    ax.set_yticks([230,250,260,270,290,300,310,330,340,350], minor=True)
-    ax.set_xticks([0,400,800,1200,1600])
-    ax.set_xticks([100,200,300,500,600,700,900,1000,1100,1300,1400,1500], minor=True)
+    ax.set_xlim(-100, 1550)
+    ax.xaxis.set_major_locator(MultipleLocator(400))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    
+    ax.set_ylim(220,380)
+    ax.yaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+    
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
     ax.yaxis.set_ticks_position("both")
 
-    ax.set_ylabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_xlabel(r"$\mathregular{\rho}$ (kg/m$\mathregular{^3}$)", fontsize=32, labelpad=10)
+    ax.set_ylabel("T (K)", fontsize=32, labelpad=10)
+    ax.set_xlabel(r"$\mathregular{\rho}$ (kg/m$\mathregular{^3}$)", fontsize=32, labelpad=20)
     for axis in ['top','bottom','left','right']:
         ax.spines[axis].set_linewidth(2.0)
 
     fig.tight_layout()
-    fig.savefig("pdfs/fig1-vle-r125.pdf")
+    fig.savefig("pdfs/fig3-vle-r125.pdf")
 
     # Plot Vapor Pressure
     fig, ax = plt.subplots()
@@ -92,21 +127,34 @@ def main():
             df.filter(regex=(f"Pvap_{float(temp):.0f}K")),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
+    ax.scatter(
+        df_gaff["T_K"],
+        df_gaff["pvap_bar"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
     ax.scatter(
         R125.expt_Pvap.keys(),
         R125.expt_Pvap.values(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
 
-    ax.set_xlim(220,320)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([220,230,240,250,260,270,290,300,310], minor=True)
-    ax.set_yticks([0,10,20])
-    ax.set_yticks([2,4,6,8,12,14,16,18,22,24,26], minor=True)
+    ax.set_xlim(220,330)
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+
+    ax.set_ylim(0,40)
+    ax.yaxis.set_major_locator(MultipleLocator(10))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
@@ -119,7 +167,7 @@ def main():
 
 
     fig.tight_layout()
-    fig.savefig("pdfs/fig1-pvap-r125.pdf")
+    fig.savefig("pdfs/fig3-pvap-r125.pdf")
 
     # Plot Enthalpy of Vaporization
     fig, ax = plt.subplots()
@@ -129,21 +177,34 @@ def main():
             df.filter(regex=(f"Hvap_{float(temp):.0f}K")),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
+    ax.scatter(
+        df_gaff["T_K"],
+        df_gaff["hvap_kJmol"] / R125.molecular_weight * 1000.0,
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
     ax.scatter(
         R125.expt_Hvap.keys(),
         R125.expt_Hvap.values(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
 
-    ax.set_xlim(220,320)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([220,230,240,250,260,270,290,300,310], minor=True)
-    ax.set_yticks([100, 125, 150, 175])
-    ax.set_yticks([85,90,95,105,110,115,120,130,135,140,145,155,160,165,170], minor=True)
+    ax.set_xlim(220,330)
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+
+    ax.set_ylim(90,410)
+    ax.yaxis.set_major_locator(MultipleLocator(100))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
@@ -155,151 +216,35 @@ def main():
         ax.spines[axis].set_linewidth(2.0)
 
     fig.tight_layout()
-    fig.savefig("pdfs/fig1-hvap-r125.pdf")
+    fig.savefig("pdfs/fig3-hvap-r125.pdf")
 
-    df["mape_norm_tot"] = (
-        df["mape_Tc"] / df["mape_Tc"].max() +
-        df["mape_rhoc"] / df["mape_rhoc"].max() +
-        df["mape_Pvap"] / df["mape_Pvap"].max() +
-        df["mape_Hvap"] / df["mape_Hvap"].max()
-    )
-    topn = df.sort_values("mape_norm_tot")[:5]
 
-    # Plot VLE envelopes
-    clrs = seaborn.color_palette('bright', n_colors=len(topn))
+def calc_critical(df):
+    """Compute the critical temperature and density
 
-    fig, ax = plt.subplots()
-    temps = R125.expt_liq_density.keys()
-    for temp in temps:
-        ax.scatter(
-            topn.filter(regex=(f"liq_density_{float(temp):.0f}K")),
-            np.tile(temp, len(topn)),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-        ax.scatter(
-            topn.filter(regex=(f"vap_density_{float(temp):.0f}K")),
-            np.tile(temp, len(topn)),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        topn.filter(regex=("sim_rhoc")),
-        topn.filter(regex=("sim_Tc")),
-        c=clrs,
-        s=180,
-        alpha=0.2,
-    )
-    ax.scatter(
-        R125.expt_liq_density.values(),
-        R125.expt_liq_density.keys(),
-        color="black",
-        marker="x",
-        s=240,
-    )
-    ax.scatter(
-        R125.expt_vap_density.values(),
-        R125.expt_vap_density.keys(),
-        color="black",
-        marker="x",
-        s=240,
-    )
-    ax.scatter(R125.expt_rhoc, R125.expt_Tc, color="black", marker="x", s=240)
+    Accepts a dataframe with "T_K", "rholiq_kgm3" and "rhovap_kgm3"
+    Returns the critical temperature (K) and density (kg/m3)
 
-    ax.set_ylim(220,360)
-    ax.set_yticks([240, 280, 320, 360])
-    ax.set_yticks([230,250,260,270,290,300,310,330,340,350], minor=True)
-    ax.set_xticks([0,400,800,1200,1600])
-    ax.set_xticks([100,200,300,500,600,700,900,1000,1100,1300,1400,1500], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
-
-    ax.set_ylabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_xlabel(r"$\mathregular{\rho}$ (kg/m$\mathregular{^3}$)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig1-vle-top-r125.pdf")
-
-    # Plot Vapor Pressure
-    fig, ax = plt.subplots()
-
-    for temp in temps:
-        ax.scatter(
-            np.tile(temp, len(topn)),
-            topn.filter(regex=(f"Pvap_{float(temp):.0f}K")),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        R125.expt_Pvap.keys(),
-        R125.expt_Pvap.values(),
-        color="black",
-        marker="x",
-        s=240,
+    Computes the critical properties with the law of rectilinear diameters
+    """
+    temps = df["T_K"].values
+    liq_density = df["rholiq_kgm3"].values
+    vap_density = df["rhovap_kgm3"].values
+    # Critical Point (Law of rectilinear diameters)
+    slope1, intercept1, r_value1, p_value1, std_err1 = linregress(
+        temps,
+        (liq_density + vap_density) / 2.0,
     )
 
-    ax.set_xlim(220,320)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([220,230,240,250,260,270,290,300,310], minor=True)
-    ax.set_yticks([0,10,20])
-    ax.set_yticks([2,4,6,8,12,14,16,18,22,24,26], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
-
-    ax.set_xlabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_ylabel(r"$\mathregular{P_{vap}}$ (bar)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig1-pvap-top-r125.pdf")
-
-    # Plot Enthalpy of Vaporization
-    fig, ax = plt.subplots()
-    for temp in temps:
-        ax.scatter(
-            np.tile(temp, len(topn)),
-            topn.filter(regex=(f"Hvap_{float(temp):.0f}K")),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        R125.expt_Hvap.keys(),
-        R125.expt_Hvap.values(),
-        color="black",
-        marker="x",
-        s=240,
+    slope2, intercept2, r_value2, p_value2, std_err2 = linregress(
+        temps,
+        (liq_density - vap_density) ** (1 / 0.32),
     )
 
-    ax.set_xlim(220,320)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([220,230,240,250,260,270,290,300,310], minor=True)
-    ax.set_yticks([100, 125, 150, 175])
-    ax.set_yticks([85,90,95,105,110,115,120,130,135,140,145,155,160,165,170], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
+    Tc = np.abs(intercept2 / slope2)
+    rhoc = intercept1 + slope1 * Tc
 
-    ax.set_xlabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_ylabel(r"$\mathregular{\Delta H_{vap}}$ (kJ/kg)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig1-hvap-top-r125.pdf")
-
+    return Tc, rhoc
 
 if __name__ == "__main__":
     main()
--- hfcs-fffit/analysis/final-figs/plotfig3-r32.py
+++ hfcs-fffit/analysis/final-figs/plotfig3-r32.py
@@ -7,7 +7,9 @@ import seaborn
 
 sys.path.append("../")
 
+from scipy.stats import linregress
 from utils.r32 import R32Constants
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 
 R32 = R32Constants()
 
@@ -16,7 +18,7 @@ matplotlib.rc("font", serif="Arial")
 
 df = pd.read_csv("../csv/r32-pareto.csv", index_col=0)
 df_gaff = pd.read_csv("../csv/r32-gaff.csv", index_col=0)
-df_rabbe = pd.read_csv("../csv/r32-rabbe.csv", index_col=0)
+df_raabe = pd.read_csv("../csv/r32-raabe.csv", index_col=0)
 
 
 def main():
@@ -34,43 +36,104 @@ def main():
             np.tile(temp, len(df)),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
         ax.scatter(
             df.filter(regex=(f"vap_density_{float(temp):.0f}K")),
             np.tile(temp, len(df)),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
     ax.scatter(
         df.filter(regex=("sim_rhoc")),
         df.filter(regex=("sim_Tc")),
         c=clrs,
         s=180,
-        alpha=0.1,
+        alpha=0.2,
     )
+
+    tc, rhoc = calc_critical(df_gaff)
+    ax.scatter(
+        df_gaff["rholiq_kgm3"],
+        df_gaff["T_K"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
+    ax.scatter(
+        df_gaff["rhovap_kgm3"],
+        df_gaff["T_K"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        marker='s',
+    )
+    ax.scatter(
+        rhoc,
+        tc,
+        c='gray',
+        s=120,
+        alpha=0.7,
+        marker='s',
+    )
+
+    tc, rhoc = calc_critical(df_raabe)
+    ax.scatter(
+        df_raabe["rholiq_kgm3"],
+        df_raabe["T_K"],
+        c='#0a4091',
+        s=120,
+        alpha=0.7,
+        label="Raabe",
+        marker='^',
+    )
+    ax.scatter(
+        df_raabe["rhovap_kgm3"],
+        df_raabe["T_K"],
+        c='#0a4091',
+        s=120,
+        alpha=0.7,
+        label="Raabe",
+        marker='^',
+    )
+    ax.scatter(
+        rhoc,
+        tc,
+        c='#0a4091',
+        s=120,
+        alpha=0.7,
+        marker='^',
+    )
+
     ax.scatter(
         R32.expt_liq_density.values(),
         R32.expt_liq_density.keys(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
     ax.scatter(
         R32.expt_vap_density.values(),
         R32.expt_vap_density.keys(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
-    ax.scatter(R32.expt_rhoc, R32.expt_Tc, color="black", marker="x", s=240)
+    ax.scatter(R32.expt_rhoc, R32.expt_Tc, color="black", marker="x", linewidths=2, s=200)
 
-    ax.set_ylim(230,370)
-    ax.set_yticks([240, 280, 320, 360])
-    ax.set_yticks([250,260,270,290,300,310,330,340,350], minor=True)
-    ax.set_xticks([0,400,800,1200])
-    ax.set_xticks([100,200,300,500,600,700,900,1000,1100,1300], minor=True)
+    ax.set_xlim(-100, 1550)
+    ax.xaxis.set_major_locator(MultipleLocator(400))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    
+    ax.set_ylim(220,380)
+    ax.yaxis.set_major_locator(MultipleLocator(40))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+    
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
@@ -93,21 +156,43 @@ def main():
             df.filter(regex=(f"Pvap_{float(temp):.0f}K")),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
+    ax.scatter(
+        df_gaff["T_K"],
+        df_gaff["pvap_bar"],
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
+    ax.scatter(
+        df_raabe["T_K"],
+        df_raabe["pvap_bar"],
+        c='#0a4091',
+        s=120,
+        alpha=0.7,
+        label="Raabe",
+        marker='^',
+    )
     ax.scatter(
         R32.expt_Pvap.keys(),
         R32.expt_Pvap.values(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
 
-    ax.set_xlim(230,330)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([250,260,270,290,300,310,330], minor=True)
-    ax.set_yticks([0,10,20,30])
-    ax.set_yticks([2,4,6,8,12,14,16,18,22,24,26,28,32,34], minor=True)
+    ax.set_xlim(220,330)
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+
+    ax.set_ylim(0,40)
+    ax.yaxis.set_major_locator(MultipleLocator(10))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
@@ -130,21 +215,43 @@ def main():
             df.filter(regex=(f"Hvap_{float(temp):.0f}K")),
             c=clrs,
             s=180,
-            alpha=0.1,
+            alpha=0.2,
         )
+    ax.scatter(
+        df_gaff["T_K"],
+        df_gaff["hvap_kJmol"] / R32.molecular_weight * 1000.0,
+        c='gray',
+        s=120,
+        alpha=0.7,
+        label="GAFF",
+        marker='s',
+    )
+    ax.scatter(
+        df_raabe["T_K"],
+        df_raabe["hvap_kJmol"] / R32.molecular_weight * 1000.0,
+        c='#0a4091',
+        s=120,
+        alpha=0.7,
+        label="Raabe",
+        marker='^',
+    )
     ax.scatter(
         R32.expt_Hvap.keys(),
         R32.expt_Hvap.values(),
         color="black",
         marker="x",
-        s=240,
+        linewidths=2,
+        s=200,
     )
 
-    ax.set_xlim(230,330)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([250,260,270,290,300,310,330], minor=True)
-    ax.set_yticks([200, 250, 300, 350])
-    ax.set_yticks([210,220,230,240,260,270,280,390,310,320,330,340,360,370,380], minor=True)
+    ax.set_xlim(220,330)
+    ax.xaxis.set_major_locator(MultipleLocator(40))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+
+    ax.set_ylim(90,410)
+    ax.yaxis.set_major_locator(MultipleLocator(100))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
+
     ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
     ax.tick_params("both", which="major", length=8)
     ax.xaxis.set_ticks_position("both")
@@ -158,149 +265,33 @@ def main():
     fig.tight_layout()
     fig.savefig("pdfs/fig3-hvap-r32.pdf")
 
-    df["mape_norm_tot"] = (
-        df["mape_Tc"] / df["mape_Tc"].max() +
-        df["mape_rhoc"] / df["mape_rhoc"].max() +
-        df["mape_Pvap"] / df["mape_Pvap"].max() +
-        df["mape_Hvap"] / df["mape_Hvap"].max()
-    )
-    topn = df.sort_values("mape_norm_tot")[:5]
 
-    # Plot VLE envelopes
-    clrs = seaborn.color_palette('bright', n_colors=len(topn))
+def calc_critical(df):
+    """Compute the critical temperature and density
 
-    fig, ax = plt.subplots()
-    temps = R32.expt_liq_density.keys()
-    for temp in temps:
-        ax.scatter(
-            topn.filter(regex=(f"liq_density_{float(temp):.0f}K")),
-            np.tile(temp, len(topn)),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-        ax.scatter(
-            topn.filter(regex=(f"vap_density_{float(temp):.0f}K")),
-            np.tile(temp, len(topn)),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        topn.filter(regex=("sim_rhoc")),
-        topn.filter(regex=("sim_Tc")),
-        c=clrs,
-        s=180,
-        alpha=0.2,
-    )
-    ax.scatter(
-        R32.expt_liq_density.values(),
-        R32.expt_liq_density.keys(),
-        color="black",
-        marker="x",
-        s=240,
-    )
-    ax.scatter(
-        R32.expt_vap_density.values(),
-        R32.expt_vap_density.keys(),
-        color="black",
-        marker="x",
-        s=240,
-    )
-    ax.scatter(R32.expt_rhoc, R32.expt_Tc, color="black", marker="x", s=240)
+    Accepts a dataframe with "T_K", "rholiq_kgm3" and "rhovap_kgm3"
+    Returns the critical temperature (K) and density (kg/m3)
 
-    ax.set_ylim(230,370)
-    ax.set_yticks([240, 280, 320, 360])
-    ax.set_yticks([250,260,270,290,300,310,330,340,350], minor=True)
-    ax.set_xticks([0,400,800,1200])
-    ax.set_xticks([100,200,300,500,600,700,900,1000,1100,1300], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
-
-    ax.set_ylabel("T (K)", fontsize=32, labelpad=10)
-    ax.set_xlabel(r"$\mathregular{\rho}$ (kg/m$\mathregular{^3}$)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig3-vle-top-r32.pdf")
-
-    # Plot Vapor Pressure
-    fig, ax = plt.subplots()
-
-    for temp in temps:
-        ax.scatter(
-            np.tile(temp, len(topn)),
-            topn.filter(regex=(f"Pvap_{float(temp):.0f}K")),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        R32.expt_Pvap.keys(),
-        R32.expt_Pvap.values(),
-        color="black",
-        marker="x",
-        s=240,
+    Computes the critical properties with the law of rectilinear diameters
+    """
+    temps = df["T_K"].values
+    liq_density = df["rholiq_kgm3"].values
+    vap_density = df["rhovap_kgm3"].values
+    # Critical Point (Law of rectilinear diameters)
+    slope1, intercept1, r_value1, p_value1, std_err1 = linregress(
+        temps,
+        (liq_density + vap_density) / 2.0,
     )
 
-    ax.set_xlim(230,330)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([250,260,270,290,300,310,330], minor=True)
-    ax.set_yticks([0,10,20,30])
-    ax.set_yticks([2,4,6,8,12,14,16,18,22,24,26,28,32,34], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
-
-    ax.set_xlabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_ylabel(r"$\mathregular{P_{vap}}$ (bar)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig3-pvap-top-r32.pdf")
-
-    # Plot Enthalpy of Vaporization
-    fig, ax = plt.subplots()
-    for temp in temps:
-        ax.scatter(
-            np.tile(temp, len(topn)),
-            topn.filter(regex=(f"Hvap_{float(temp):.0f}K")),
-            c=clrs,
-            s=180,
-            alpha=0.2,
-        )
-    ax.scatter(
-        R32.expt_Hvap.keys(),
-        R32.expt_Hvap.values(),
-        color="black",
-        marker="x",
-        s=240,
+    slope2, intercept2, r_value2, p_value2, std_err2 = linregress(
+        temps,
+        (liq_density - vap_density) ** (1 / 0.32),
     )
 
-    ax.set_xlim(230,330)
-    ax.set_xticks([240, 280, 320])
-    ax.set_xticks([250,260,270,290,300,310,330], minor=True)
-    ax.set_yticks([200, 250, 300, 350])
-    ax.set_yticks([210,220,230,240,260,270,280,390,310,320,330,340,360,370,380], minor=True)
-    ax.tick_params("both", direction="in", which="both", length=4, labelsize=26, pad=10)
-    ax.tick_params("both", which="major", length=8)
-    ax.xaxis.set_ticks_position("both")
-    ax.yaxis.set_ticks_position("both")
+    Tc = np.abs(intercept2 / slope2)
+    rhoc = intercept1 + slope1 * Tc
 
-    ax.set_xlabel("T (K)", fontsize=32, labelpad=20)
-    ax.set_ylabel(r"$\mathregular{\Delta H_{vap}}$ (kJ/kg)", fontsize=32, labelpad=10)
-    for axis in ['top','bottom','left','right']:
-        ax.spines[axis].set_linewidth(2.0)
-
-
-    fig.tight_layout()
-    fig.savefig("pdfs/fig3-hvap-top-r32.pdf")
-
+    return Tc, rhoc
 
 if __name__ == "__main__":
     main()
