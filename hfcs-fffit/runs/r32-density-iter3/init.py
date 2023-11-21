f419a0c15c9fb06ca6348f62396d8e235e386e9b
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2023-11-21 11:51:55 -0500

    Update all r32 iterations

--- hfcs-fffit/runs/r32-density-iter3/init.py
+++ hfcs-fffit/runs/r32-density-iter3/init.py
@@ -8,10 +8,16 @@ from fffit.utils import values_scaled_to_real
 def init_project():
 
     # Initialize project
-    project = signac.init_project("r32-density-iter3")
+    project = signac.init_project()
 
     # Define temps
-    temps = [241.0 * u.K, 261.0 * u.K, 281.0 * u.K, 301.0 * u.K, 321.0 * u.K]
+    temps = [
+        241.0 * u.K,
+        261.0 * u.K,
+        281.0 * u.K,
+        301.0 * u.K,
+        321.0 * u.K
+    ]
 
     # Run at vapor pressure
     press = {
@@ -27,16 +33,10 @@ def init_project():
     nstepsprod = 2500000
 
     # Load samples from Latin hypercube
-    lh_samples = np.genfromtxt(
-        "../../analysis/csv/r32-density-iter3-params.csv",
-        delimiter=",",
-        skip_header=1,
-    )[:, 1:]
+    lh_samples = np.genfromtxt("data/lh_samples_200_r32.txt")
 
     # Define bounds on sigma/epsilon
-    bounds_sigma = np.asarray(
-        [[3.0, 4.0], [2.5, 3.5], [1.7, 2.7],]
-    )  # C  # F  # H
+    bounds_sigma = np.asarray([[3.0, 4.0], [2.5, 3.5], [1.7, 2.7],])  # C  # F  # H
 
     bounds_epsilon = np.asarray(
         [[20.0, 60.0], [15.0, 40.0], [2.0, 10.0],]  # C  # F  # H
@@ -51,35 +51,20 @@ def init_project():
         for sample in scaled_params:
 
             # Unpack the sample
-            (
-                sigma_C,
-                sigma_F,
-                sigma_H,
-                epsilon_C,
-                epsilon_F,
-                epsilon_H,
-            ) = sample
+            (sigma_C, sigma_F, sigma_H, epsilon_C, epsilon_F, epsilon_H) = sample
 
             # Define the state point
             state_point = {
                 "T": float(temp.in_units(u.K).value),
-                "P": float(
-                    press[int(temp.in_units(u.K).value)].in_units(u.bar).value
-                ),
+                "P": float(press[int(temp.in_units(u.K).value)].in_units(u.bar).value),
                 "nstepseq": nstepseq,
                 "nstepsprod": nstepsprod,
                 "sigma_C": float((sigma_C * u.Angstrom).in_units(u.nm).value),
                 "sigma_F": float((sigma_F * u.Angstrom).in_units(u.nm).value),
                 "sigma_H": float((sigma_H * u.Angstrom).in_units(u.nm).value),
-                "epsilon_C": float(
-                    (epsilon_C * u.K * u.kb).in_units("kJ/mol").value
-                ),
-                "epsilon_F": float(
-                    (epsilon_F * u.K * u.kb).in_units("kJ/mol").value
-                ),
-                "epsilon_H": float(
-                    (epsilon_H * u.K * u.kb).in_units("kJ/mol").value
-                ),
+                "epsilon_C": float((epsilon_C * u.K * u.kb).in_units("kJ/mol").value),
+                "epsilon_F": float((epsilon_F * u.K * u.kb).in_units("kJ/mol").value),
+                "epsilon_H": float((epsilon_H * u.K * u.kb).in_units("kJ/mol").value),
             }
 
             job = project.open_job(state_point)
